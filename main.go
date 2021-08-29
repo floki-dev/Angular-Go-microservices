b2fa6a17eb1a96ae1810a3327d459523617c4090
Fedor <doworkupwell@gmail.com>
2021-08-29 03:14:58 +0300

    db

--- main.go
+++ main.go
@@ -3,6 +3,12 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
+  _, err := gorm.Open(postgres.Open("root:root@tcp(postgres:5432)/ambassador"), &gorm.Config{})
+
+  if err != nil {
+    panic("Could not connect with the database!")
+  }
+
   app := fiber.New()
 
   app.Get("/", func(c *fiber.Ctx) error {
